bac0128d16342810f9ae3a345d8a2af68b845230
11907223 <e.vaniterson98@gmail.com>
2024-09-04 08:55:09 +0200

    502 bad gateway oops

---
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -0,0 +1,30 @@
+<?php
+
+// Load Composer's autoloader
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Load environment variables from .env file
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+define('DB_NAME', getenv('DB_NAME'));
+define('DB_USER', getenv('DB_USER'));
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
+define('DB_HOST', getenv('DB_HOST'));
+define('DB_CHARSET', getenv('DB_CHARSET'));
+
+// define('AUTH_KEY', getenv('AUTH_KEY'));
+// define('SECURE_AUTH_KEY', getenv('SECURE_AUTH_KEY'));
+// define('LOGGED_IN_KEY', getenv('LOGGED_IN_KEY'));
+// define('NONCE_KEY', getenv('NONCE_KEY'));
+// define('AUTH_SALT', getenv('AUTH_SALT'));
+// define('SECURE_AUTH_SALT', getenv('SECURE_AUTH_SALT'));
+
+$table_prefix  = 'wp_';
+
+define('WP_DEBUG', false);
+
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+require_once(ABSPATH . 'wp-settings.php');
