7c6b201ffd4934b7d004daf9aaf12c22c450b6a0
elise <e.vaniterson98@gmail.com>
2024-09-05 16:42:20 +0200

    nginx works

--- srcs/requirements/tools/test_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$mysqli = new mysqli(getenv('DB_HOST'), getenv('DB_USER'), getenv('DB_PASSWORD'), getenv('DB_NAME'));
-
-if ($mysqli->connect_error) {
-    die("Connection failed: " . $mysqli->connect_error);
-}
-echo "Connected successfully";
-?>
