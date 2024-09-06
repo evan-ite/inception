fcdfc00eb77b41aea6db0446446421c9cb8e8150
elise <e.vaniterson98@gmail.com>
2024-09-06 15:25:37 +0200

    wordpress and mariadb are linked and first databse is created

--- srcs/requirements/mariadb/conf/setup-db.sh
+++ srcs/requirements/mariadb/conf/setup-db.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
 # Check if variables are set
-if [ -z "$DB_USER" ] || [ -z "$MYSQL_ROOT_PASSWORD" ] || [ -z "$MYSQL_PASSWORD" ]; then
+if [ -z "$DB_USER" ] || [ -z "$MYSQL_ROOT_PASSWORD" ] || [ -z "$DB_PASSWORD" ] || [ -z "$DB_NAME" ]; then
     echo "One or more environment variables are missing in the .env file."
     exit 1
 fi
 
+# Create Database for WordPress
+mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "
+CREATE DATABASE IF NOT EXISTS ${DB_NAME};
+CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+
 # Connect to MariaDB and grant privileges
 mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "
-GRANT ALL PRIVILEGES ON *.* TO '${DB_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON *.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' WITH GRANT OPTION;
 FLUSH PRIVILEGES;"
 
 # Check if the privileges were granted successfully
 if [ $? -eq 0 ]; then
-    echo "Privileges granted successfully to ${DB_USER} for ${WP_CONTAINER_NAME}."
+    echo "Privileges granted successfully to ${MYSQL_USER}."
 else
     echo "Failed to grant privileges."
 fi
