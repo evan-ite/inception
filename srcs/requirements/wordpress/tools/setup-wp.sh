4b4c2e9da47f732b96a614668753539da82cb696
elise <e.vaniterson98@gmail.com>
2024-09-06 16:31:19 +0200

    users and admin created for wp

---
+++ srcs/requirements/wordpress/tools/setup-wp.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Check if WordPress is already installed
+if [ ! -f /var/www/html/wordpress/wp-config.php ]; then
+
+    wp config create \
+        --dbname=$DB_NAME \
+        --dbuser=$DB_USER \
+        --dbpass=$DB_PASSWORD \
+        --dbhost=$DB_HOST \
+        --path=/var/www/html/wordpress \
+        --allow-root
+	
+    wp core install \
+        --url=$DOMAIN_NAME \
+        --title=$WP_TITLE \
+        --admin_user=$WP_ADMIN \
+        --admin_email=$WP_ADMIN_EMAIL \
+        --admin_password=$WP_ADMIN_PASSWORD \
+        --path=/var/www/html/wordpress \
+        --allow-root
+else    
+    # Update site title
+    wp option update blogname "$WP_TITLE" --path=/var/www/html/wordpress --allow-root
+
+    # Update admin user info
+    wp user update $WP_ADMIN \
+        --user_pass=$WP_ADMIN_PASSWORD \
+        --user_email=$WP_ADMIN_EMAIL \
+        --path=/var/www/html/wordpress \
+        --allow-root
+
+fi
+
+# Create additional user
+wp user create $WP_USER $WP_USER_EMAIL \
+    --user_pass=$WP_USER_PASSWORD \
+    --role=editor \
+    --path=/var/www/html/wordpress \
+    --allow-root
